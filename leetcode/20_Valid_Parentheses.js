fec748a48c6e1cc3e3ce40021af76dac12d04767
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-04 19:33:01 -0400

    20

---
+++ leetcode/20_Valid_Parentheses.js
@@ -0,0 +1,22 @@
+// Problem #: 20
+// Title: Valid Parentheses
+// Difficulty: Easy
+const isValid = (s) => {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] in map) {
+            stack.push(s[i]);
+        } else {
+            if(stack.length === 0) return false;
+            if(map[stack.pop()] !== s[i]) return false;
+        }
+    }
+    
+    return stack.length === 0;
+}
