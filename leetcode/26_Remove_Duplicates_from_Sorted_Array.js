17db74108edb370618958da9278ca344ae55a09b
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-05 08:43:29 -0400

    26

---
+++ leetcode/26_Remove_Duplicates_from_Sorted_Array.js
@@ -0,0 +1,13 @@
+// Problem #: 26
+// Title: Remove Duplicates from Sorted Array
+// Difficulty: Easy
+const removeDuplicates = (nums) => {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
