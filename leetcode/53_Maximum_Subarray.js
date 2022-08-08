d4075ef761ef9b538017ab2d9840fed774c67448
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-07 20:24:51 -0400

    53

---
+++ leetcode/53_Maximum_Subarray.js
@@ -0,0 +1,12 @@
+// Problem #: 53
+// Title: Maximum Subarray
+// Difficulty: Medium
+const maxSubArray = (nums) => {
+    let max = nums[0];
+    let current = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        current = Math.max(nums[i], current + nums[i]);
+        max = Math.max(max, current);
+    }
+    return max;
+}
