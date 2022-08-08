0c7d79fa571976566d558279296597e12bca6f14
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-08 09:24:50 -0400

    300

---
+++ leetcode/300_Longest_Increasing_Subsequence.js
@@ -0,0 +1,15 @@
+// Problem #: 300
+// Title: Longest Increasing Subsequence
+// Difficulty: Medium
+const lengthOfLIS = (nums) => {
+    let dp = [];
+    for (let i = 0; i < nums.length; i++) {
+        dp[i] = 1;
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    return Math.max(...dp);
+}
