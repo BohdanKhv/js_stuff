7805746244a887b8e1bd89f7c0461a97e73ccaa9
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-08 19:26:03 -0400

    83

--- leetcode/83. Remove Duplicates from Sorted List.js
+++ leetcode/83. Remove Duplicates from Sorted List.js
@@ -3,15 +3,21 @@
 // Difficulty: Easy
 var deleteDuplicates = function(head) {
     if(!head) return null;
-    
+
+    // create a new node to store the result
     let curr = head;
-    
+
+    // loop through the list
     while(curr.next){
+        // if the current node's value is equal to the next node's value
         if (curr.val === curr.next.val)
+            // delete the next node
             curr.next = curr.next.next;
         else
+            // otherwise, move to the next node
             curr = curr.next;
     }
-    
+
+    // return the result node (the first node)
     return head;
 }
