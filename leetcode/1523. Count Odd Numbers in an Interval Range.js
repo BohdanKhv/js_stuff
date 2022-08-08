7d7359971bfd0f91afefbf54d6f3c2e6de67af9f
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-08 16:59:41 -0400

    1524

---
+++ leetcode/1523. Count Odd Numbers in an Interval Range.js
@@ -0,0 +1,20 @@
+// Problem #: 1523
+// Problem Name: Count Odd Numbers in an Interval Range
+// Difficulty: Easy
+var countOdds = function(low, high) {
+    let counter = 0;
+    
+    for(let i = low; i <=  high; i++) {
+        if(i % 2 !== 0) counter++;
+    }
+        
+    return counter;
+};
+
+// Faster Solution
+var countOdds = function(low, high) {
+    if(low % 2 === 0 &&  high % 2 === 0) {
+        return (high - low) / 2;
+    }
+    return parseInt((high - low) / 2) + 1
+};
