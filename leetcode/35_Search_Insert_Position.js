55b6aecbfd5925498794655479e8e400c22a97a4
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-09 16:56:15 -0400

    35

--- leetcode/35_Search_Insert_Position.js
+++ leetcode/35_Search_Insert_Position.js
@@ -1,7 +1,7 @@
 // Problem #: 35
 // Title: Search Insert Position
 // Difficulty: Easy
-const searchInsert = (nums, target) => {
+var searchInsert = (nums, target) => {
     let i = 0;
     while (i < nums.length && nums[i] < target) {
         i++;
@@ -10,9 +10,24 @@ const searchInsert = (nums, target) => {
 }
 
 // Faster solution
-var searchInsert2 = function(nums, target) {
+var searchInsert = function(nums, target) {
     const q = nums.filter(n => n < target);
     const index = nums.indexOf(q[q.length-1])
     
     return index + 1
+};
+
+// Binary Search Solution
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    
+    return left
 };
