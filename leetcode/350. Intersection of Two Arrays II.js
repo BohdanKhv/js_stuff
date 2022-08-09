fb540b637a97ba01e63b4d894cc25d52b590a7c7
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-09 12:43:15 -0400

    350

--- leetcode/350. Intersection of Two Arrays II.js
+++ leetcode/350. Intersection of Two Arrays II.js
@@ -29,3 +29,25 @@ var intersect = function(nums1, nums2) {
     }
     return result;
 }
+
+// Same solution
+var intersect = function(nums1, nums2) {
+    let map = {};
+    let result = [];
+
+    for(let i = 0; i < nums1.length; i++) {
+        if(map[nums1[i]] === undefined)
+            map[nums1[i]] = 1
+        else
+            map[nums1[i]] += 1
+    }
+
+    for(let i = 0; i < nums2.length; i++) {
+        if(map[nums2[i]]){
+            map[nums2[i]] -= 1;
+            result.push(nums2[i])
+        }
+    }
+    
+    return result;
+};
