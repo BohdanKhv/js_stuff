86c2f27c50a238740b456f17974b37570a026881
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-10 19:36:22 -0400

    367

---
+++ leetcode/367. Valid Perfect Square.js
@@ -0,0 +1,16 @@
+// Problem #: 367
+// Title: Valid Perfect Square
+// Difficulty: Easy
+var isPerfectSquare = function(num) {
+    let left = 1;
+    let right = num;
+    
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(mid * mid === num) return true;
+        else if(mid * mid > num) right = mid - 1;
+        else left = mid + 1;
+    }
+    
+    return false;
+};
