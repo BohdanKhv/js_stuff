f98ffd492ecd87d5baa455e735ae63a015489c0d
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-12 09:38:39 -0400

    50

--- leetcode/50. Pow(x, n).js
+++ leetcode/50. Pow(x, n).js
@@ -5,6 +5,11 @@ var myPow = function(x, n) {
     if (n === 0) return 1;
     if (n === 1) return x;
     if (n === -1) return 1 / x;
+    
+    // if n is even then x^n = x^(n/2) * x^(n/2)
     if (n % 2 === 0) return myPow(x * x, n / 2);
+
+    // if n is odd then x^n = x^(n/2) * x^(n/2) * x
+    // Recursive call
     return x * myPow(x * x, (n - 1) / 2);
 }
