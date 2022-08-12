fef385842b219bab530fb74c7ea6dfc1767da2b0
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-12 09:37:22 -0400

    50

--- leetcode/50. Pow(x, n).js
+++ leetcode/50. Pow(x, n).js
@@ -6,5 +6,5 @@ var myPow = function(x, n) {
     if (n === 1) return x;
     if (n === -1) return 1 / x;
     if (n % 2 === 0) return myPow(x * x, n / 2);
-    return x * myPow(x * x, n / 2);
+    return x * myPow(x * x, (n - 1) / 2);
 }
