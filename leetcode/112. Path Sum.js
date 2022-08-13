a45534670b3c6dd030e86d97a358d37f7d120919
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-13 13:12:52 -0400

    112

---
+++ leetcode/112. Path Sum.js
@@ -0,0 +1,34 @@
+// Problem #: 112
+// Title: Path Sum
+// Difficulty: Easy
+var hasPathSum = function(root, targetSum) {
+    if(!root) return false;
+
+    let hasPath = false;
+
+    // Using depth first search to find the path
+    const dfs = (node, sum) => {
+        if(hasPath) return
+        // If we reach the leaf node, check if the sum is equal to the target sum
+        if(!node.left && !node.right && sum === node.val) hasPath = true;
+        
+        // Decrement the sum by the value of the current node
+        sum = sum - node.val;
+        
+        // Keep searching for the path
+        if(node.left) dfs(node.left, sum);
+        if(node.right) dfs(node.right, sum);
+    }
+
+    // Start the search from the root node
+    dfs(root, targetSum);
+
+    return hasPath;
+};
+
+// Recursive solution (Faster)
+var hasPathSum = function(root, targetSum) {
+    if(!root) return false;
+    if(!root.left && !root.right && targetSum === root.val) return true;
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+}
