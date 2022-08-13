143ab857db633399ef68eec8c81a40363b1e7afc
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-13 09:15:47 -0400

    700

--- leetcode/700. Search in a Binary Search Tree.js
+++ leetcode/700. Search in a Binary Search Tree.js
@@ -9,23 +9,23 @@ var searchBST = function(root, val) {
 
     while(queue.length > 0) {
         const current = queue.shift();
-        
+
         if(current.val === val) return current;
-        
+
         if(current.left) queue.push(current.left);
         if(current.right) queue.push(current.right);
     }
-    
+
     return null
 };
 
 
-// Other solutions
+// Traverse solutions
 var searchBST = function(root, val) {
     if(!root) return null
-    
+
     let result = null;
-    
+
     // => Left | => Middle | => Right
     const traverse = (node) => {
         if(node.left) traverse(node.left);
@@ -34,8 +34,15 @@ var searchBST = function(root, val) {
         
         if(node.right) traverse(node.right);
     }
-    
+
     traverse(root);
-    
+
     return result;
+};
+
+// Recursive solution
+var searchBST = function(root, val) {
+    if(!root) return null;
+    if(root.val === val) return root;
+    return searchBST(root.right, val) || searchBST(root.left, val);
 };
