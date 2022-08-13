28bbf7f0fd66010027e9ee7c16f293d13982d70d
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-13 09:58:34 -0400

    404

--- leetcode/404. Sum of Left Leaves.js
+++ leetcode/404. Sum of Left Leaves.js
@@ -3,10 +3,10 @@
 // Difficulty: Easy
 var sumOfLeftLeaves = function(root) {
     if(!root) return 0
-    
+
     const stack = [root];
     let counter = 0;
-    
+
     while(stack.length > 0) {
         const current = stack.pop();
 
@@ -21,3 +21,31 @@ var sumOfLeftLeaves = function(root) {
 
     return counter;
 };
+
+// Traverse solution
+var sumOfLeftLeaves = function(root) {
+    if(!root) return 0
+
+    let counter = 0;
+
+    const traverse = (node) => {
+        if(node.left) {
+            if(!node.left.left && !node.left.right) counter += node.left.val;
+            traverse(node.left);
+        }
+        
+        if(node.right) traverse(node.right)
+    }
+
+    traverse(root);
+
+    return counter;
+};
+
+// Recursive solution
+var sumOfLeftLeaves = function(root) {
+    if(!root) return 0;
+    if(!root.left && !root.right) return 0;
+    if(root.left && !root.left.left && !root.left.right) return root.left.val + sumOfLeftLeaves(root.right);
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+}
