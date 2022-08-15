6b0fd115dc1b2f03dbfebe7bb7454c4b9ee91f3f
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-15 11:04:21 -0400

    13

--- leetcode/13. Roman to Integer.js
+++ leetcode/13. Roman to Integer.js
@@ -1,7 +1,7 @@
 // Problem #: 14
 // Title: Longest Common Prefix
 // Difficulty: Easy
-const romanToInt = (roman) => {
+var romanToInt = (roman) => {
     const romanMap = {
         I: 1,
         V: 5,
@@ -23,3 +23,29 @@ const romanToInt = (roman) => {
 
     return sum;
 }
+
+// Other solution:
+
+var romanToInt = function(s) {
+    const hashMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    let sum = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        let prev = hashMap[s[i - 1]];
+        sum += hashMap[s[i]];
+        
+        if(prev && prev < hashMap[s[i]])
+            sum -= prev * 2;
+    }
+
+    return sum;
+};
