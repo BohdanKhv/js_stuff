5695e57b5d71e4c5cdecb2218f537aa161254797
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-18 17:13:15 -0400

    216

---
+++ leetcode/216. Combination Sum III.js
@@ -0,0 +1,33 @@
+// Problem #: 216
+// Title: Combination Sum III
+// Difficulty: Medium
+var combinationSum3 = function(k, n) {
+    // Global result
+    const result = [];
+    
+    const nums = [];
+    for(let i = 1; i < 10; i++) nums.push(i);
+    
+    const bfs = (i, nums, k, n, slate) => {
+        // Backtracking case
+        if(n < 0) return
+        
+        // Base case
+        if(slate.length === k) {
+            if(n === 0)
+                result.push(slate.slice())
+            return
+        }
+        
+        //dfs recursive case
+        for(let j = i; j < nums.length; j++) {
+            slate.push(nums[j]);
+            bfs(j + 1, nums, k, n - nums[j], slate);
+            slate.pop();
+        }
+    }
+    
+    bfs(0, nums, k, n, []);
+    
+    return result;
+};
