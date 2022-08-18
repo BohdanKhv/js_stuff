20bbd94026a8ec74ae3dda35fedc20f6a22709a3
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-17 20:26:15 -0400

    1768

--- leetcode/1768. Merge Strings Alternately.js
+++ leetcode/1768. Merge Strings Alternately.js
@@ -5,7 +5,7 @@ var mergeAlternately = function(word1, word2) {
     let str = '';
     let count1 = 0;
     let count2 = 0;
-    
+
     for(let i = 0; i < word1.length + word2.length; i++) {
         if((i % 2 === 0 && count1 < word1.length) || count2 === word2.length) {
             str = str + word1[count1];
@@ -15,6 +15,6 @@ var mergeAlternately = function(word1, word2) {
             count2++;
         }
     }
-    
+
     return str;
 };
