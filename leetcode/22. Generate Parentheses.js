5e79dd6deca8bd78de8b4651cded238c3de5fa18
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-21 17:19:03 -0400

    22 second solution

--- leetcode/22. Generate Parentheses.js
+++ leetcode/22. Generate Parentheses.js
@@ -1,7 +1,7 @@
 // Problem #: 22
 // Problem name: Generate Parentheses
 // Problem description:
-const generateParenthesis = function(n) {
+var generateParenthesis = function(n) {
     let result = [];
     // base case
     if(n === 0) return result;
@@ -13,7 +13,7 @@ const generateParenthesis = function(n) {
     }
 }
 
-const generateParenthesisHelper = function(left, right, str, result) {
+var generateParenthesisHelper = function(left, right, str, result) {
     // base case
     if(left === 0 && right === 0) {
         result.push(str);
@@ -29,3 +29,37 @@ const generateParenthesisHelper = function(left, right, str, result) {
         }
     }
 }
+
+
+// Other solutions:
+var generateParenthesis = function(n) {
+    // Global result
+    const result = [];
+
+    // dfs recursive helper
+    const dfs = (i, n, slate, oCount, cCount) => {
+        // backtracking case
+        if(oCount < cCount) return;
+        if(oCount > n ) return;
+
+        // Base case
+        if(i === n*2) {
+            result.push(slate.join(''));
+
+            return
+        }
+
+        // add open paren
+        slate.push('(');
+        dfs(i + 1, n, slate, oCount + 1, cCount);
+        slate.pop();
+        
+        // add close paren
+        slate.push(')');
+        dfs(i + 1, n, slate, oCount, cCount + 1);
+        slate.pop();
+    }
+    
+    dfs(0, n, [], 0, 0)
+    return result;
+};
